270a1fc745d6a3bf3be66b21fa21970f76e8be5f
Grant Ales <grant.ales@yahoo.com>
2021-11-23 16:41:28 -0700

    changes

--- Queue.md
+++ Queue.md
@@ -71,9 +71,102 @@ We will be going back and coding out the ice cream scenario and create the line 
 * Dont add a person to the line if the line is over 10 people
 * If out of ice cream end the program
 
-I will be giving supplying the function names and some other stuff in the program to make it easier. Here are the functions and how they will be used in the program.
+I will be giving supplying the function names and some other stuff in the program to make it easier. Here are the functions and how they will be used in the program. Here is the template:
+
 ``` python
-TODO: program
+class Ice_cream_truck:
+    def __init__(self,max_size, stock):
+        self.stock = stock
+        self.max_size = max_size
+
+        self.line = []
+
+    def add_to_line(self, name):
+        """
+        Purpose: Add a person to the end of the line
+        with the name given as an argument.
+        """
+        pass
+
+
+    def remove_first_in_line(self):
+        """
+        Purpose: Remove the first person in the line.
+        If someone is removed from the line we know that
+        an ice cream is given away. Since everyone
+        is only allowed 1, deduct from the stock here.
+        """
+        pass
+
+    
+    def get_line_size(self):
+        """
+        Purpose: Returns the size of the line
+        """
+        return len(self.line)
+
+
+    def out_of_ice_cream(self):
+        """
+        Purpose: Return a boolean to see if the truck
+        is out of ice cream.
+        """
+        return self.stock == 0
+
+
+    def __str__(self):
+        """
+        Purpose: Loop through self.line and grab the names of all
+        who are in the line. Return everyone in the line.
+        """
+        names = ""
+        for name in self.line:
+            names += name + " "
+
+        return names
+
+
+stock = 12
+line = Ice_cream_truck(10, stock)
+
+# ============== Test 1 ============== #
+line.add_to_line("Grant") 
+line.add_to_line("Jeremy")
+line.add_to_line("Breanna")
+line.add_to_line("Hunter")
+line.add_to_line("John")
+line.add_to_line("Wyatt")
+print(line) # Output: Grant Jeremy Breanna Hunter John Wyatt
+
+# ============== Test 2 ============== #
+line.remove_first_in_line()
+line.remove_first_in_line()
+print(line) # Output: Breanna Hunter John Wyatt
+
+line.add_to_line("Ed")
+line.add_to_line("Dawson")
+line.add_to_line("Spencer")
+line.add_to_line("Will")
+line.add_to_line("Lori")
+line.add_to_line("Amber")
+print(line)  # Output: Breanna Hunter John Wyatt Ed Dawson Spencer Will Lori Amber
+
+
+# ============== Test 3 ============== #
+line.add_to_line("Lauren")
+line.add_to_line("Linda")
+print(line)  # Output: Breanna Hunter John Wyatt Ed Dawson Spencer Will Lori Amber
+
+
+for person in range(line.get_line_size()):
+    """
+    We will then remove everyone from the line and give them
+    ice cream.
+    """
+    if line.out_of_ice_cream():
+        print("Have a great day guys!!")
+
+    line.remove_first_in_line()
 ``` 
 
 
--- Queue_solution.py
+++ Queue_solution.py
@@ -8,10 +8,7 @@ class Ice_cream_truck:
     def add_to_line(self, name):
         """
         Purpose: Add a person to the end of the line
-        with the name given as an argument. If there happens to be 
-        0 people in the line then the ice cream truck will go to the next
-        neighborhood. Return the string "Have a good day" if the line is 
-        empty.
+        with the name given as an argument.
         """
         pass
 
@@ -19,27 +16,82 @@ class Ice_cream_truck:
     def remove_first_in_line(self):
         """
         Purpose: Remove the first person in the line.
-        Add a condition in the function to return a string saying "Have a good day"
-        if the line is empty since you can remove nothing and the driver is
-        going to a new neighborhood. Everyone will only be allowed 1 ice cream each.
-        Deduct from the stock before you remove a person from the line.
-        This function will only manipulate the line, stock, or return the given string.
+        If someone is removed from the line we know that
+        an ice cream is given away. Since everyone
+        is only allowed 1, deduct from the stock here.
         """
         pass
+
+    
+    def get_line_size(self):
+        """
+        Purpose: Returns the size of the line
+        """
+        return len(self.line)
+
 
     def out_of_ice_cream(self):
         """
         Purpose: Return a boolean to see if the truck
-        is out of ice cream or not. Return the string
-        "Have a good day" if out of ice_cream.
+        is out of ice cream.
         """
         return self.stock == 0
 
-    def __str__(self):
-        print(self.line)
 
-def main():
-    stock = 12
-    ice_cream_truck_line = Ice_cream_truck(10, stock)
+    def __str__(self):
+        """
+        Purpose: Loop through self.line and grab the names of all
+        who are in the line. Return everyone in the line.
+        """
+        names = ""
+        for name in self.line:
+            names += name + " "
+
+        return names
+
+
+stock = 12
+line = Ice_cream_truck(10, stock)
+
+# ============== Test 1 ============== #
+line.add_to_line("Grant") 
+line.add_to_line("Jeremy")
+line.add_to_line("Breanna")
+line.add_to_line("Hunter")
+line.add_to_line("John")
+line.add_to_line("Wyatt")
+print(line) # Output: Grant Jeremy Breanna Hunter John Wyatt
+
+# ============== Test 2 ============== #
+line.remove_first_in_line()
+line.remove_first_in_line()
+print(line) # Output: Breanna Hunter John Wyatt
+
+line.add_to_line("Ed")
+line.add_to_line("Dawson")
+line.add_to_line("Spencer")
+line.add_to_line("Will")
+line.add_to_line("Lori")
+line.add_to_line("Amber")
+print(line)  # Output: Breanna Hunter John Wyatt Ed Dawson Spencer Will Lori Amber
+
+
+# ============== Test 3 ============== #
+line.add_to_line("Lauren")
+line.add_to_line("Linda")
+print(line)  # Output: Breanna Hunter John Wyatt Ed Dawson Spencer Will Lori Amber
+
+
+for person in range(line.get_line_size()):
+    """
+    We will then remove everyone from the line and give them
+    ice cream.
+    """
+    if line.out_of_ice_cream():
+        print("Have a great day guys!!")
+
+    line.remove_first_in_line()
+
+
     
-    print(ice_cream_truck_line)
+
